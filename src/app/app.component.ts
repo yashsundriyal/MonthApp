7de17a0a69338f41153457be10afd1c3a20a406e
unknown <yashsundriyal4000@gmail.com>
2025-09-13 19:38:22 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'month-app';
+months = [
+  { name: 'June', images: ['https://picsum.photos/seed/june/400/300'] },
+  { name: 'July', images: ['https://picsum.photos/seed/july/400/300'] },
+  { name: 'August', images: ['https://picsum.photos/seed/aug/400/300'] }
+];
+
+  activeIndex = 0;
+
+  setActive(i: number) {
+    this.activeIndex = i;
+    const container = document.querySelector('.carousel') as HTMLElement;
+    const slideWidth = container.clientWidth;
+    container.scrollTo({ left: i * slideWidth, behavior: 'smooth' });
+  }
+
+  onScroll(event: Event) {
+    const el = event.target as HTMLElement;
+    const i = Math.round(el.scrollLeft / el.clientWidth);
+    this.activeIndex = i;
+  }
 }
